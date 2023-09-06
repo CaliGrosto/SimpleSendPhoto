cd348d816739def46bdc687a33cb7b177cdd4666
徐冠 <caligrosto@qq.com>
2023-09-06 11:07:22 +0800

    new

--- src/main/java/com/alins/GroupEventListener.java
+++ src/main/java/com/alins/GroupEventListener.java
@@ -18,75 +18,48 @@ public class GroupEventListener extends SimpleListenerHost {
     int[] ints = null;
     @EventHandler
     public void GroupEvent(@NotNull MessageEvent event) {
-        TimeMeterUtil timeMeterUtil = new TimeMeterUtil();
-
-        boolean NoPutBackSampling = config.INSTANCE.getNoPutBackSampling();
-
         String content = event.getMessage().contentToString();
         List<String> trigger = config.INSTANCE.getTrigger();
-        List<String> picturePath = config.INSTANCE.getPicturePath();
 
         int size = trigger.size();
         for (int t = 0; t < size; t++){
-            if (config.INSTANCE.getFuzzyMatching()){
+            if(config.INSTANCE.getFuzzyMatching()){
                 if (content.equals(trigger.get(t))){
-                    //启动计时器
-                    if(timeMeterUtil.getLocalPicSecond() == 0){
-                        if (NoPutBackSampling){
-                            if (number == -1){
-                                //数组重置
-                                ints = SendPicUtil.GetRandomArray(picturePath.get(t));
-                                number = 0;
-                            }
-
-                            SendPicUtil.SendNoPutBackPic(ints,number,picturePath.get(t),event);
-                            number++;
-
-                            if (number > SendPicUtil.getFilePathLength(picturePath.get(t))-1){
-                                number = -1;
-                            }
-                        }else{
-                            SendPicUtil.SendRandomPic(picturePath.get(t), event);
-                        }
-
-                        timeMeterUtil.LocalSecond(config.INSTANCE.getSelfPicCD());
-
-                        break;
-
-                    }else{
-                        event.getSubject().sendMessage(config.INSTANCE.getCdPointOut());
-                    }
-                }
-            }else{
-                if (content.contains(trigger.get(t))){
-                    //启动计时器
-                    if(timeMeterUtil.getLocalPicSecond() == 0){
-                        if (NoPutBackSampling){
-                            if (number == -1){
-                                //数组重置
-                                ints = SendPicUtil.GetRandomArray(picturePath.get(t));
-                                number = 0;
-                            }
-
-                            SendPicUtil.SendNoPutBackPic(ints,number,picturePath.get(t),event);
-                            number++;
-
-                            if (number > SendPicUtil.getFilePathLength(picturePath.get(t))-1){
-                                number = -1;
-                            }
-                        }else{
-                            SendPicUtil.SendRandomPic(picturePath.get(t), event);
-                        }
-
-                        timeMeterUtil.LocalSecond(config.INSTANCE.getSelfPicCD());
-
-                        break;
-
-                    }else{
-                        event.getSubject().sendMessage(config.INSTANCE.getCdPointOut());
-                    }
+                    Shit(t,event);
+                }else if(content.contains(trigger.get(t))){
+                    Shit(t,event);
                 }
             }
+        }
+    }
+
+    /**
+     * 码如其名，就是依托淳谢特，就是淳懒，主打一个能用就行
+     * @param t
+     */
+    public void Shit(int t,MessageEvent event){
+        TimeMeterUtil timeMeterUtil = new TimeMeterUtil();
+        List<String> picturePath = config.INSTANCE.getPicturePath();
+        boolean NoPutBackSampling = config.INSTANCE.getNoPutBackSampling();
+        //启动计时器
+        if(timeMeterUtil.getLocalPicSecond() == 0){
+            if (NoPutBackSampling){
+                if (number == -1){
+                    //数组重置
+                    ints = SendPicUtil.GetRandomArray(picturePath.get(t));
+                    number = 0;
+                }
+                SendPicUtil.SendNoPutBackPic(ints,number,picturePath.get(t),event);
+                number++;
+                if (number > SendPicUtil.getFilePathLength(picturePath.get(t))-1){
+                    number = -1;
+                }
+            }else{
+                SendPicUtil.SendRandomPic(picturePath.get(t), event);
+            }
+            timeMeterUtil.LocalSecond(config.INSTANCE.getSelfPicCD());
+        }else{
+            event.getSubject().sendMessage(config.INSTANCE.getCdPointOut());
         }
     }
 }
